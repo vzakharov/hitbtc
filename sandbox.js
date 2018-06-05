43096b55ed41f4adbd2c3093bdd53877b95b9457
Vova <vzakharov@gmail.com>
2018-06-05 18:32:25 +0300

    .

--- sandbox.js
+++ sandbox.js
@@ -16,6 +16,7 @@ async function main() {
     let hitbtc = new Hitbtc()
 
     let trades = await hitbtc.api.public.trades('EMCBTC', {limit: 1000})
+    let {length} = trades
 
     for (let trade of trades) {
 
@@ -26,10 +27,28 @@ async function main() {
             ) / 60000,
 
             area: Math.sqrt(trade.quantity)
-
         })
 
     }
+
+    let addDerivative = (property, derivativeOf, order) => {
+
+        for (let i = 0; i < length - order; i++) {
+
+            let trade = trades[i]
+            let previousTrade = trades[i + 1]
+    
+            trade[property] = 
+                (trade[derivativeOf] - previousTrade[derivativeOf]) / 
+                (trade.time - previousTrade.time)
+    
+        }
+    
+    }
+
+    addDerivative('speed', 'price', 1)
+    addDerivative('acceleration', 'speed', 2)
+
 
     fs.writeFileSync('./sandbox/trades.tsv', json2csv.parse(trades, {delimiter: '\t'}))
 
